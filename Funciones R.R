3a895c5f44f2c06ec9b585f45f258307e0d359b8
ariamp21 <ariamp21@gmail.com>
2019-04-12 19:35:53 -0400

    carpeta nueva de funciones en R

---
+++ Funciones R.R
@@ -0,0 +1,11 @@
+ funcionUno <- function(){
+  ## aqui va el desarrollo de la funcion 
+  print ("Hola Mundo")
+  }
+
+ 
+funcionUno()
+  
+# cuando la funcion no aparezca puedo poner source(nombre que le puse al guardarlo en la carpeta)
+
+  
