51987fd89960ea47545d10bbcb85ad9fb8da22f5
ariamp21 <ariamp21@gmail.com>
2019-04-13 11:04:45 -0400

    Actualizacion de receta de diseño

--- Funciones R.R
+++ Funciones R.R
@@ -1,11 +1,50 @@
- funcionUno <- function(){
-  ## aqui va el desarrollo de la funcion 
-  print ("Hola Mundo")
-  }
+ funcionUno <- function(x,y){
+   
+  ## aqui va el desarrollo de la funcion. si modifico la funcion y la ejecuto al otro lado, tengo que volver a ejecutarla
+   
+   print("ahora voy a sumar")
+   print(x+y)
+}
 
+ # el ultimo valor q se realiza, es el valor que retornará.  tb se puede q el print(x+y) puedo eliminar el print y sus parentesis 
  
 funcionUno()
   
 # cuando la funcion no aparezca puedo poner source(nombre que le puse al guardarlo en la carpeta)
 
+# Receta de diseño:
+
+# Realizar el desarrollo de la velocidad de un cuerpo en relacion al tiempo, donde Veloc = Dist/Tiempo
+# poner valores de entrada y salida
+
+# function velocidad(Double , Double) -> Double 
+
+# Test velocidad(10,2) -> 5
+
+
+funcionVelocidad <- function(d,t){
+  print("ahora voy a dividir")
+  print(d/t) 
   
+  
+  
+  
+}
+# Realizar funcion resto con tres variables
+
+funcionResto <- function(x, y, z){
+  print ("ahora haré la funcion resto")
+ x*y-(z*trunc((x*y)/z))
+  
+  
+}
+funcionResto(3,4,7)
+
+print("otra forma de realizarlo")
+
+resto <- function(x,y,z){
+  
+  (x*y)%%z
+}
+
+VariableResto <- resto(3,4,7)
